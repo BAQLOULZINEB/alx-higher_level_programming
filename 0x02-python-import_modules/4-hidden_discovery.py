c70cb30fef43fcd4db5730c423500d574b397c60
BAQLOULZINEB <zinebbaq@gmail.com>
2024-03-14 17:25:03 -0700

    last

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
-# #!/usr/bin/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
-    import hidden_4
+    from hidden_4 import *
 
-    names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
-            print(name)
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
